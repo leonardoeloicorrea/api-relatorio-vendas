b0ac93ac66dd024100ff03103321a2e3f1de107e
Leonardo correa <leonardoeloi24@gmail.com>
2023-12-14 20:51:56 -0300

    Created api and postgres config

---
+++ src/main/java/com/leonardo/apirelatoriovendas/ApiRelatorioVendasApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.apirelatoriovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRelatorioVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRelatorioVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/apirelatoriovendas/ApiRelatorioVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.apirelatoriovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRelatorioVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
